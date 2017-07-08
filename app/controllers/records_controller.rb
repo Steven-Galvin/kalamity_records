630afe0cb899c9721a36f9e4f5d059934b873ac7
Steven Galvin <steven.john.galvin@gmail.com>
2017-07-07 19:16:14 -0700

    add full crud functionality with authentication and authorization across records and reviews.

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,4 +6,48 @@ class RecordsController < ApplicationController
   def show
     @record = Record.find(params[:id])
   end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+    flash[:notice] = "Record successfully added!"
+      redirect_to  record_path(@record)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      flash[:notice] = "Record successfully updated!"
+      redirect_to record_path(@record)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.reviews.each do |review|
+      review.destroy
+    end
+    if @record.destroy
+      flash[:notice] = "Record successfully removed!"
+      redirect_to records_path
+    end
+  end
+
+private
+  def record_params
+    params.require(:record).permit(:title, :artist, :price, :description, :album_length, :genre)
+  end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,7 @@
 class Record < ApplicationRecord
   has_many :reviews
 
-  validates :title, :price, :description, :genre, :album_length, presence: true
+  validates :artist, :title, :price, :description, :genre, :album_length, presence: true
+  validates :artist, :title, :genre, length: { in: 3..25 }
+  validates :description, length: { maximum: 500 }
 end
